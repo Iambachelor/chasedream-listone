f14b1c481cfb7cf690fcf4ecdbbe1234f2226087
15083267362@163.com <15083267362@163.com>
2020-11-25 23:49:36 +0800

    2020-11-25 mybatis入门-mybatis配置

---
+++ src/main/java/cn/dreamchase/dreamchaselistone/App.java
@@ -0,0 +1,13 @@
+package cn.dreamchase.dreamchaselistone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
